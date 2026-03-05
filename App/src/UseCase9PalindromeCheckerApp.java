59520909fb88e67b2b52d3c6bb79287e07db85bd
Animesh Jha <aj0970@srmist.edu.in>
2026-03-05 21:40:20 +0530

    UC9

---
+++ App/src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Define input string
+        String input = "madam";
+
+        // Call recursive function
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    // Recursive method to check palindrome
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
+    }
+}
