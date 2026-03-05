4c0779c4c44b4c39aa518371d2e0a87c9223c29e
Animesh Jha <aj0970@srmist.edu.in>
2026-03-05 21:45:04 +0530

    UC11

---
+++ App/src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        // Create object of service class
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+
+// Service class containing palindrome logic
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        // Initialize pointers
+        int start = 0;
+        int end = input.length() - 1;
+
+        // Compare characters moving inward
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
